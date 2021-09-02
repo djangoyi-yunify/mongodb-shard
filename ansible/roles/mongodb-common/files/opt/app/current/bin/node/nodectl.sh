2a9e4d6ec2fb71b855d8bf00b1b47a22d2a90668
Yi Jing <djangoyi@yunify.com>
2021-09-02 15:16:50 +0800

    tmpl files: ready for starting cluster

--- ansible/roles/mongodb-common/files/opt/app/current/bin/node/nodectl.sh
+++ ansible/roles/mongodb-common/files/opt/app/current/bin/node/nodectl.sh
@@ -1,12 +1,14 @@
 # sourced by /opt/app/current/bin/ctl.sh
-
+MONGODB_DATA_PATH=/data/mongodb-data
+MONGODB_LOG_PATH=/data/mongodb-logs
+MONGODB_CONF_PATH=/data/mongodb-conf
 start() {
     #_start
     log "service started"
 }
 
 init() {
-    log "init cluster"
+    log "init replicaset"
 }
 
 checkConfdChange() {
